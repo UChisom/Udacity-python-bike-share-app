cc2fb16207dfc654c1f1451b7ec55fb34b9597e5
Chisom <chisom.urom@gmail.com>
2022-04-27 10:44:16 +0100

    Rephrase doc string for "get_filters" function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Requests user to specify a city, month, and day for analysis.
 
     Returns:
         (str) city - name of the city to analyze
